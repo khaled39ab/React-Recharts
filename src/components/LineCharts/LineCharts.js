709690128be63dcd53708a0f0c9953b310c0d9d9
khaled39ab <khaled39ab@gmail.com>
2022-05-25 23:44:04 +0600

    create charts with recharts

---
+++ src/components/LineCharts/LineCharts.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const LineCharts = () => {
+    const data = [
+        {
+          name: 'Page A',
+          red: 4000,
+          green: 2400,
+          blue: 2400,
+        },
+        {
+          name: 'Page B',
+          red: 3000,
+          green: 1398,
+          blue: 2210,
+        },
+        {
+          name: 'Page C',
+          red: 2000,
+          green: 9800,
+          blue: 2290,
+        },
+        {
+          name: 'Page D',
+          red: 2780,
+          green: 3908,
+          blue: 2000,
+        },
+        {
+          name: 'Page E',
+          red: 1890,
+          green: 4800,
+          blue: 2181,
+        },
+        {
+          name: 'Page F',
+          red: 2390,
+          green: 3800,
+          blue: 2500,
+        },
+        {
+          name: 'Page G',
+          red: 3490,
+          green: 4300,
+          blue: 2100,
+        },
+      ];
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default LineCharts;
